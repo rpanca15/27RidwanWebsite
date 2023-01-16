100a725ccbfe45e46bb53896e6b61c604cd8180b
rpanca15 <122759577+rpanca15@users.noreply.github.com>
2023-01-16 09:25:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Main</title>
+</head>
+
+<body>
+    <a href="layout1.html"><button>Layout 1</button></a>
+    <a href="layout2.html"><button>Layout 2</button></a>
+    <a href="layout3.html"><button>Layout 3</button></a>
+    <a href="layout4.html"><button>Layout 4</button></a>
+    <a href="layout5.html"><button>Layout 5</button></a>
+    <a href="layout6.html"><button>Layout 6</button></a>
+</body>
+</html>
